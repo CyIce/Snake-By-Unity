6db80ad9d8e9b8d27d41dcc7767fc87fea7058b7
CyIce <1207201395@qq.com>
2016-11-05 16:33:06 +0800

    2016.11.5.16

--- Assets/Scripts/SnakeControl.cs
+++ Assets/Scripts/SnakeControl.cs
@@ -8,7 +8,7 @@ public class SnakeControl : MonoBehaviour
     public GameObject[] snake = new GameObject[10];
 
     //用于记录snake各个部位的位置和运动方向,[ ,0]记录运动方向；
-    private Vector3[,] snakePos = new Vector3[10, 3];
+    private Vector3[,] snakePos = new Vector3[10, 2];
 
     //snake的长度；
     private int snakeSize;
@@ -25,6 +25,8 @@ public class SnakeControl : MonoBehaviour
     void Update()
     {
         controlSnake();
+
+        follow();
 
     }
 
@@ -79,5 +81,34 @@ public class SnakeControl : MonoBehaviour
         }     
     }
 
+    //控制snake的身体跟随snake的头；
+    void follow()
+    {
+        int i;
+        //用于储存两段body之间的向量差（除去body的半径）；
+        Vector3 disPos;
+
+        //用于储存下一段body需要移动的向量；
+        Vector3 movePos;
+
+        for(i=2;i<=snakeSize;i++)
+        {
+            disPos = snake[i - 1].transform.position - snake[i].transform.position;
+
+            disPos -= (disPos / Vector3.Magnitude(disPos));
+
+            movePos = Vector3.Project(disPos, snakePos[i, 0]);
+
+            //移动各个body的位置；
+            snake[i].transform.position += movePos;
+
+            snakePos[i, 0] = snake[i].transform.position - snakePos[i, 1];
+            snakePos[i, 1] = snake[i].transform.position;
+
+        }
+
+
+
+    }
 
 }
